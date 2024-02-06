66ceb5e949dda3525e9f00256b05ee2742cc706c
Tadej Vengust <vengust.tadej@gmail.com>
2024-02-06 14:02:20 +0100

    set production config

--- lib/config/production.ts
+++ lib/config/production.ts
@@ -2,10 +2,10 @@ import type { ConfigInterface } from '~/lib/types/general.types';
 
 const config: ConfigInterface = {
   API_BASE: 'https://api.apillon.io',
-  CHAIN_ID: '1', // Ethereum Mainnet
-  CONTRACT_ADDRESS: '', // Contract address
-  CLAIM_START: 0,
-  CLAIM_END: 0,
+  CHAIN_ID: '11155111', // Sepolia (Ethereum testnet)
+  CONTRACT_ADDRESS: '0x95A64F0d16C2Fb779BD83B3F8Fb72ae1c08da877', // Contract address
+  CLAIM_START: 1707221128,
+  CLAIM_END: 1707379528,
 };
 
 export default config;
