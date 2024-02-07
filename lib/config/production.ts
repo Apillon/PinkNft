4e7380c592317c7bace7bf57cfb6a6dd0beb84a1
Tadej Vengust <vengust.tadej@gmail.com>
2024-02-07 22:23:35 +0100

    update contract

--- lib/config/production.ts
+++ lib/config/production.ts
@@ -3,9 +3,7 @@ import type { ConfigInterface } from '~/lib/types/general.types';
 const config: ConfigInterface = {
   API_BASE: 'https://api.apillon.io',
   CHAIN_ID: '11155111', // Sepolia (Ethereum testnet)
-  CONTRACT_ADDRESS: '0x430DcC95536CD20bF3C54792C0078A341805a778', // Contract address
-  CLAIM_START: 1707315321,
-  CLAIM_END: 1707316221,
+  CONTRACT_ADDRESS: '0xbA4f576e5393c43400E1142eF8c2A2b0881aE908', // Contract address
+  CLAIM_START: 1707337193,
+  CLAIM_END: 1707398393,
 };
-
-export default config;
